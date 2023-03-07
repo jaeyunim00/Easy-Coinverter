bb0fabc40a007a3a9fe88fe7666da90867c1528d
jaeyunim00 <ijy00120877@gmail.com>
2023-03-07 23:44:51 +0900

    dev search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
+  const [coins, setCoins] = React.useState([]);
+  const [coin, setCoin] = React.useState("");
+
+  const regex = new RegExp(`${coin}`, "gi");
+
+  const handleCoinChange = (event) => {
+    event.preventDefault();
+    setCoin(event.target.value);
+    console.log(coin);
+  };
+
+  const handleCoinSubmit = (event) => {
+    event.preventDefault();
+    const coinNameArr = coins.map((coin) => {
+      return coin.name;
+    });
+
+    console.log(regex);
+    const filteredArr = coinNameArr.filter((item) => regex.test(item));
+    console.log(filteredArr);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => setCoins(json));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="header">
+        <span class="material-symbols-outlined">currency_bitcoin</span>
+        <span>Coinverter</span>
+      </div>
+      <div class="coin_input">
+        <span>TyPe cOiN</span>
+        <form method="get" onSubmit={handleCoinSubmit}>
+          <input type="text" value={coin} onChange={handleCoinChange} />
+        </form>
+      </div>
+      <div class="coin_list">
+        {coins.map((coin) => {
+          return <div key={coin.id}>{coin.name}</div>;
+        })}
+      </div>
+      <h1></h1>
     </div>
   );
 }
